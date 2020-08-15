346802d57dc565017ff54880470ba6b4f4540773
mohinimishra <asmyselfmohini@gmail.com>
2020-08-15 20:13:27 +0530

    added express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/view/index.html')
+})
+
+
+app.listen(3000, () => console.log('Server up n running on port 3000'))
